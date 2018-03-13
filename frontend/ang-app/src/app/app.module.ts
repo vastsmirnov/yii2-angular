586736d9cee101201761a1e40512ce92b32f43ca
Michael Rychagov <mixrich1@gmail.com>
2018-03-13 12:04:17 +0700

    issue 2 - Add articles-list component

--- frontend/ang-app/src/app/app.module.ts
+++ frontend/ang-app/src/app/app.module.ts
@@ -5,13 +5,15 @@ import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
 import {HeaderComponent} from "./components/header/header.component";
 import {FooterComponent} from "./components/footer/footer.component";
+import { ArticlesListComponent } from './articles-list/articles-list.component';
 
 
 @NgModule({
     declarations: [
         HeaderComponent,
         FooterComponent,
-        AppComponent
+        AppComponent,
+        ArticlesListComponent
     ],
     imports: [
         BrowserModule
